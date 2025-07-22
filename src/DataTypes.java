c216b25709da312486c42765f1ce143185de8c76
dyesmuk <dyesmuk@gmail.com>
2025-07-22 00:35:22 -0700

    initial commit

---
+++ src/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        System.out.println("Data types in Java");
+
+        int id = 10; // integer value
+        String name = "Sonu Singh"; // String value
+        double salary = 90000.75; // floating point value 
+
+
+    }
+
+}
---
+++ src/Vaman.java
@@ -0,0 +1,11 @@
+public class Vaman {
+
+//    main
+    public static void main(String[] args) {
+
+//        sout
+        System.out.println("Hello world!");
+
+    }
+
+}
