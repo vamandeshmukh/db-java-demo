648e5fad2c91915702281d7848d33ac42fb9a644
dyesmuk <dyesmuk@gmail.com>
2025-07-22 16:18:31 +0530

    initial commit

--- src/SwitchDemo.java
+++ src/SwitchDemo.java
@@ -1,2 +1,29 @@
+
+// Find an adjective based on the letter given
+
 public class SwitchDemo {
+
+    // A - Attractive , B - Beautiful, C - Cute
+
+    public static void main(String[] args) {
+
+        String letter = "V";
+
+        switch (letter) {
+            case "A":
+                System.out.println("Attractive");
+                break;
+            case "B":
+                System.out.println("beautiful");
+                break;
+            case "C":
+                System.out.println("Cute");
+                break;
+            default:
+                System.out.println("Other");
+        }
+
+
+
+    }
 }
