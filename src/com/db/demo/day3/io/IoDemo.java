1c92db7f1b7b7b0a639dba4d8629561d38b7a191
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-23 17:00:34 +0530

    demo commit

---
+++ src/com/db/demo/day3/io/IoDemo.java
@@ -0,0 +1,18 @@
+package com.db.demo.day3.io;
+
+import java.io.FileReader;
+
+public class IoDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Start");
+
+//        FileReader reader = new FileReader("sample.txt");
+
+
+
+
+        System.out.println("End");
+    }
+}
