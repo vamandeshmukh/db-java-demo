efd99275618b375f7698a335d265238b4016980d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-28 10:48:35 +0530

    day 3

--- src/com/db/demo/day3/col/CollectionDemo.java
+++ src/com/db/demo/day3/col/CollectionDemo.java
@@ -1,10 +1,41 @@
 package com.db.demo.day3.col;
 
+import java.util.ArrayList;
+
 public class CollectionDemo {
 
     public static void main(String[] args) {
 
-        
+//        array
+        Integer num = 10; // 10, 20, 30
+        Integer[] numArray = {10, 20, 30}; //
+//        Integer[] numArray = {10, false, "abc"}; //
+
+        // friends list
+        ArrayList friends = new ArrayList();
+        System.out.println(friends.size());
+        System.out.println(friends);
+        friends.add("Sonu");
+        friends.add("Monu");
+        friends.add("Tonu");
+        System.out.println(friends.size());
+        System.out.println(friends);
+        friends.remove(0);
+        System.out.println(friends.size());
+        System.out.println(friends);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
