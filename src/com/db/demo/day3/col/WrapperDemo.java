38b725e4a2a43a6782f6fc09ec4821c9f8930f8f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-28 10:32:17 +0530

    day 3

---
+++ src/com/db/demo/day3/col/WrapperDemo.java
@@ -0,0 +1,32 @@
+package com.db.demo.day3.col;
+
+public class WrapperDemo {
+
+    public static void main(String[] args) {
+
+//        static ClassName.methodName();
+//        non static objectName.methodName();
+//        str. call non static methods or fields
+
+        String str = "abc";
+        String str2 = str.toUpperCase();
+
+        int num = 10;
+//        num. methods are not available
+
+//        Wrapper classes
+//        int -> Integer
+//        double -> Double
+        Integer num2 = 20;
+//        num2. methods are available
+
+
+
+
+
+
+
+
+    }
+
+}
