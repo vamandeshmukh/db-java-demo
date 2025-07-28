5fe8edb602875cbb33605c64fa38a06588b45a62
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-28 14:51:51 +0530

    day 3

--- src/com/db/demo/day3/streams/StreamsDemo.java
+++ src/com/db/demo/day3/streams/StreamsDemo.java
@@ -9,23 +9,29 @@ public class StreamsDemo {
 
     public static void main(String[] args) {
 
-        List<Employee> empList = new ArrayList<>();
-
-        empList.addAll(Arrays.asList(new Employee(101, "Sonu", 95000.0),
+        List<Employee> empList = new ArrayList<>(Arrays.asList(new Employee(101, "Sonu", 95000.0),
                 new Employee(102, "Monu", 90000.00),
                 new Employee(103, "Tonu", 85000.0)));
+
         System.out.println("List out all the employees:");
         empList.forEach(e -> System.out.println(e.toString()));
 
 //        Streams API filter, map, reduce, collect, skip. limit
-        System.out.println("Employees with salary > 90000");
-        // find employees with salary > 90000
+        System.out.println("Employees with salary > 90000:");
+
         Stream<Employee> empsWithMoreSal = empList.stream().filter(emp -> emp.getSalary() > 90000.0);
         empsWithMoreSal.forEach(System.out::println);
 
+        System.out.println("Show the first two employees:");
+        empList.stream().limit(2).toList().forEach(System.out::println);
 
+        System.out.println("Skip the first employee:");
+        empList.stream().skip(1).toList().forEach(System.out::println);
 
-
+        System.out.println("print names in upper case:");
+        empList.stream().map(e -> e.getName().toUpperCase()).toList().forEach(System.out::println);
 
     }
 }
+
+//assignment - comparable, comparator, sort, Collections.sort() etc stuff
